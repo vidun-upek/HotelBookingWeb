bdf849038e1e5ec669cce3cfe44dcdc8ccb48c17
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2025-12-04 00:20:39 +0530

    Initialize Nav Bar. Yet to make the buttons active

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+  const isOwnerPath = useLocation().pathname.includes('/owner');
+
   return (
     <div>
-      
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
