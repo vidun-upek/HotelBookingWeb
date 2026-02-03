6cb3f9072b81c44870c4ba0713a5c274c7053953
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-03 22:42:44 +0530

    feat: define Room model and initialize room controller functions

---
+++ backend/controllers/roomController.js
@@ -0,0 +1,4 @@
+export const createRoom = async (req, res) => { /* Logic to be added */ };
+export const getRooms = async (req, res) => { /* Logic to be added */ };
+export const getOwnerRooms = async (req, res) => { /* Logic to be added */ };
+export const toggleRoomAvailability = async (req, res) => { /* Logic to be added */ };
