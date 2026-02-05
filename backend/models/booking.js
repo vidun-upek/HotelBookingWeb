88bcc4d2177bae88c1b82995386c2c4bc9f1e50e
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-05 21:04:38 +0530

    feat: define Booking schema for MongoDB

---
+++ backend/models/booking.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const bookingSchema = new mongoose.Schema({
+    user: { type: String, ref: 'user', required: true },
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+    hotel: { type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', required: true },
+    checkInDate: { type: Date, required: true },
+    checkOutDate: { type: Date, required: true },
+    totalPrice: { type: Number, required: true },
+    guests: { type: Number, required: true },
+    status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' },
+    paymentMethod: { type: String, default: 'pay at hotel' },
+    isPaid: { type: Boolean, default: false }
+}, { timestamps: true });
+
+const Booking = mongoose.model('Booking', bookingSchema);
+export default Booking;
