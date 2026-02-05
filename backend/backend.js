7abc7e757880981d58ba4ad02052fe564bd1fe47
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-05 21:04:29 +0530

    chore: setup cloudinary configuration and backend entry point updates

--- backend/backend.js
+++ backend/backend.js
@@ -2,30 +2,34 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import connectCloudinary from './config/cloudinary.js';
 import { clerkMiddleware } from '@clerk/express';
 import clerkWebhooks from './controllers/clerkWebhooks.js';
 import userRouter from './routes/userRoutes.js';
 import hotelRouter from './routes/hotelRoutes.js';
+import roomRouter from './routes/roomRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 connectDB();
+connectCloudinary();
 
-// Middlewares
 app.use(cors());
 app.use(clerkMiddleware());
 
-// Webhook Route (Needs to be before express.json for Svix verification)
+// Webhook Route
 app.post('/api/clerk', express.json(), clerkWebhooks);
 
-// Regular Body Parser
 app.use(express.json());
 
 // API Endpoints
 app.get('/', (req, res) => res.send('API is working'));
 app.use('/api/user', userRouter);
 app.use('/api/hotels', hotelRouter);
+app.use('/api/rooms', roomRouter);
+app.use('/api/bookings', bookingRouter);
 
 app.listen(PORT, () => {
     console.log(`🚀 Backend running on port ${PORT}`);
