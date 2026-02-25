ac7293f62f3a1b7e7c8cde73ffad999750932848
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-25 23:09:32 +0530

    feat(routes): update hotel API endpoints and routing logic

--- backend/routes/hotelroutes.js
+++ backend/routes/hotelroutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { registerHotel } from '../controllers/hotelController.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 
 const hotelRouter = express.Router();
 
-hotelRouter.post('/register', protect, registerHotel);
+hotelRouter.post('/register', protect, asyncHandler(registerHotel));
 
 export default hotelRouter;
