353bfed14909f8fe4cf81d90e0db088969151dd0
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2026-02-25 23:09:32 +0530

    feat(routes): modify user API endpoints and routing logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { getUserData, storeRecentSearchedCities } from '../controllers/userController.js';
+import asyncHandler from '../middleware/asyncHandler.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/data', protect, getUserData);
-userRouter.post('/store-search', protect, storeRecentSearchedCities);
+userRouter.get('/data', protect, asyncHandler(getUserData));
+userRouter.post('/store-search', protect, asyncHandler(storeRecentSearchedCities));
 
 export default userRouter;
